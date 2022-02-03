efc894ae691565205360c44662d84595034070a2
Connor Dunn <Connor.Dunn.IT@gmail.com>
2022-02-02 20:46:05 -0600

    Created Book Class in Class Library

---
+++ ClassesLibrary/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Book
+    {
+        //FIELD
+        //Automatic because no buisness rules
+
+        //PROPERTIES
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        //CONSTRUCTORS
+        public Book(string title, string author, int numberOfPages)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+        }
+
+        //METHODS
+
+        public override string ToString()
+        {
+            // return base.ToString();
+            return Title;
+            
+        }
+    }
+}
