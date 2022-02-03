26a6b85a41282d4922ad00292640f7f46e1b7731
Connor Dunn <Connor.Dunn.IT@gmail.com>
2022-02-02 19:05:34 -0600

    Created Customer Class

---
+++ ClassesLibrary/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Customer
+    {
+        //FIELDS - Automatic properties - no buisness rules
+
+        //Properties
+        public string StreetAdress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ContactInfo CustomerContactInfo { get; set; }
+
+    }
+}
