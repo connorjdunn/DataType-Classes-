bb656476ca21cb7e82cb527823d5de4501b9ece2
Connor Dunn <Connor.Dunn.IT@gmail.com>
2022-02-02 20:33:59 -0600

    Created Credit Card Accont Class in Class Library

---
+++ ClassesLibrary/CreditCardAccount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class CreditCardAccount
+    {
+        //FIELD - Automatic Property - No buisness rules
+
+        //Properties
+        public int AccountNumber { get; set; }
+
+        public Customer CustCredCarAccount { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public bool IsPastDue { get; set; }
+
+        public decimal AnnualInterestRate { get; set; }
+
+        //Constructor
+        public CreditCardAccount(int accountNumber, Customer custCredCarAccount, decimal balance, bool isPastDue, decimal annualInterestRate)
+        {
+            AccountNumber = accountNumber;
+            CustCredCarAccount = custCredCarAccount;
+            Balance = balance;
+            IsPastDue = isPastDue;
+            AnnualInterestRate = annualInterestRate;
+        }
+
+        //Methods
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            return string.Format("Account Number: {0:c}\n" +
+                "Customer Info: {1}\n" +
+                "Balance: {2:c}\n" +
+                "Past Due? {3}\n" +
+                "Annual Interest Rate: {4}",
+                AccountNumber,
+                CustCredCarAccount,
+                Balance,
+                IsPastDue,
+                AnnualInterestRate);
+              
+        }
+
+
+
+    }
+}
